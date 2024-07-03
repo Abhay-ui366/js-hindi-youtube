756189a6bfadecd41f2e175378c93b8d75ea74a3
Abhay-ui366 <75940824+Abhay-ui366@users.noreply.github.com>
2024-07-03 14:06:17 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14453;
+let accountEmail = 'abhay@gmail.com'
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2 // Not allowed
+
+accountEmail="hs@hc.com"
+accountPassword="67564312"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+// alert("hello")
+
+let name="hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+console.log(typeof "hitesh")
+console.log(typeof null)
+console.log(typeof undefined)
