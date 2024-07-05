6bf75a98fabc0e0c00bdb70fd0a81c370f5df9a8
Abhay-ui366 <75940824+Abhay-ui366@users.noreply.github.com>
2024-07-05 08:34:49 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// Array, objects, functions
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+
+const bigNumber=3237657364928641292837n;
+
+const heroes=["shaktiman", "nagraj", "doga"];
+
+let myObj = {
+    name: "hitesh",
+    age: "22",
+    address: "Chennai"
+}
+
+const myFunction = function() {
+    console.log("Hello world!")
+}
+
+
